1eef71b9f2a70f4e19264e564f710355cd91ee9e
Chris Palmer <chris@red-oxide.org>
2026-03-08 16:12:13 -0400

    add zwave
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

---
+++ modules/zwave.nix
@@ -0,0 +1,27 @@
+_: {
+  flake.modules.nixos.zwave = {config, ...}: {
+    sops = {
+      secrets = {
+        zwave-s0-legacy = {};
+        zwave-s2-unauthenticated = {};
+        zwave-s2-authenticated = {};
+        zwave-s2-access-control = {};
+      };
+      templates."zwave-js-keys.json" = {
+        mode = "0444";
+        content = ''
+          {
+            "securityKeys": {
+              "S0_Legacy": "${config.sops.placeholder.zwave-s0-legacy}",
+              "S2_Unauthenticated": "${config.sops.placeholder.zwave-s2-unauthenticated}",
+              "S2_Authenticated": "${config.sops.placeholder.zwave-s2-authenticated}",
+              "S2_AccessControl": "${config.sops.placeholder.zwave-s2-access-control}"
+            }
+          }
+        '';
+      };
+    };
+
+    services.zwave-js.secretsConfigFile = config.sops.templates."zwave-js-keys.json".path;
+  };
+}
