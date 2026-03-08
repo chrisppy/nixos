41328ada727bc951cb0289444e564dda39023204
Chris Palmer <chris@red-oxide.org>
2026-03-08 16:12:04 -0400

    add printing
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

---
+++ modules/printing.nix
@@ -0,0 +1,15 @@
+_: {
+  flake.modules.nixos.pc = {pkgs, ...}: {
+    services = {
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
+      printing.drivers = with pkgs; [
+        gutenprint
+        hplipWithPlugin
+      ];
+    };
+  };
+}
