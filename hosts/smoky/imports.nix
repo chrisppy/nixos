a65dbb2d047cd336c47047e497ec4f09a582bc82
Chris Palmer <chris@red-oxide.org>
2026-04-02 13:43:19 -0400

    add plymouth module
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- hosts/smoky/imports.nix
+++ hosts/smoky/imports.nix
@@ -6,6 +6,7 @@
       determinate
       hyprland
       laptop
+      plymouth
       uefi
       zswap
     ];
---
+++ modules/plymouth.nix
@@ -0,0 +1,15 @@
+_: {
+  flake.modules.nixos.plymouth = {
+    boot = {
+      plymouth.enable = true;
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "udev.log_level=3"
+        "systemd.show_status=auto"
+      ];
+      loader.timeout = 0;
+    };
+  };
+}
