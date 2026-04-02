6b97bc1ac5ec86b95d17e947c76e89dd227b38e0
Chris Palmer <chris@red-oxide.org>
2026-04-02 06:13:53 -0400

    remove flakehub substitutions
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/settings.nix
+++ modules/settings.nix
@@ -13,16 +13,13 @@ _: {
         substituters = [
           "https://hyprland.cachix.org"
           "https://install.determinate.systems"
-          "https://cache.flakehub.com"
         ];
         trusted-substituters = [
           "https://hyprland.cachix.org"
           "https://install.determinate.systems"
-          "https://cache.flakehub.com"
         ];
         trusted-public-keys = [
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
         ];
         warn-dirty = false;
       };
