14189c99a62ad737fd4882581981bc1f3503465a
Chris Palmer <chris@red-oxide.org>
2026-04-26 20:13:32 -0400

    cleanup
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -56,6 +56,7 @@ in {
       home = {
         inherit username;
         homeDirectory = "/home/${username}";
+        fonts.fontconfig.enable = true;
       };
       news.display = "silent";
       programs.home-manager.enable = true;
