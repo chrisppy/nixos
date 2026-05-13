85d7e279a636e74226c9f9311579c21636c7e588
Chris Palmer <chris@red-oxide.org>
2026-05-13 08:52:00 -0400

    switch to librewolf
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- flake.nix
+++ flake.nix
@@ -30,13 +30,6 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
   };
 
   outputs =
---
+++ modules/librewolf.nix
@@ -0,0 +1,7 @@
+_: {
+  flake.modules.homeManager.gui = {
+    programs.librewolf = {
+      enable = true;
+    };
+  };
+}
