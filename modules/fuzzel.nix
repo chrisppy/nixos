47e6cf83231f84df85689b5774e26cc07ed44285
Chris Palmer <chris@red-oxide.org>
2026-05-14 06:42:20 -0400

    cleanup fuzzel
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/fuzzel.nix
+++ modules/fuzzel.nix
@@ -10,17 +10,7 @@ _: {
         enable = true;
         settings = {
           main = {
-            # Stylix will fill in your monospace font automatically
             terminal = "${lib.getExe config.programs.kitty.package}";
-            layer = "overlay";
-            width = 35;
-            horizontal-pad = 25;
-            vertical-pad = 20;
-          };
-          border = {
-            # Stylix usually sets the border color, but we can nudge the radius
-            width = 2;
-            radius = 10; # Replicating your previous radiusRatio
           };
         };
       };
