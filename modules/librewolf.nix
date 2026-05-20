ec9acf358f3a2ac689749da629e769b5c78597ab
Chris Palmer <chris@red-oxide.org>
2026-05-20 08:54:17 -0400

    cleanup
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -1,27 +1,30 @@
 _: {
-  flake.modules.homeManager.gui = {
-    programs.librewolf = {
-      enable = true;
-      policies = {
-        BlockAboutConfig = true;
-        DefaultDownloadDirectory = "\${home}/downloads";
-        ExtensionSettings = {
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-            default_area = "menupanel";
-            private_browsing = true;
-          };
-          "addon@darkreader.org" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          "{3c078156-979c-498b-8990-85f7987dd929}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
-            installation_mode = "force_installed";
+  flake.modules.homeManager.gui =
+    { config, ... }:
+    {
+      programs.librewolf = {
+        enable = true;
+        policies = {
+          BlockAboutConfig = true;
+          DefaultDownloadDirectory = "${config.home.homeDirectory}/downloads";
+          ExtensionSettings = {
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+              default_area = "menupanel";
+              private_browsing = true;
+            };
+            "addon@darkreader.org" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            "{3c078156-979c-498b-8990-85f7987dd929}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
+              installation_mode = "force_installed";
+            };
           };
         };
       };
+      stylix.targets.librewolf.profileNames = [ "default" ];
     };
-  };
 }
