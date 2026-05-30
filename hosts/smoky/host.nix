ba8afc7165a5197cc09dac2f59ac9985ce31914b
Chris Palmer <chris@red-oxide.org>
2026-05-30 19:37:06 -0400

    update to 26.05
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- hosts/smoky/host.nix
+++ hosts/smoky/host.nix
@@ -2,7 +2,7 @@ _: {
   configurations.nixos.smoky.module = {
     hardware.facter.reportPath = ./facter.json;
     nixpkgs.hostPlatform = "x86_64-linux";
-    system.stateVersion = "25.11";
+    system.stateVersion = "26.05";
     time.timeZone = "America/New_York";
   };
 }
