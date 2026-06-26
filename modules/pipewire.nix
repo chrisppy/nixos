48061ea32333887f49b01d7b471e63bf2c1907e9
Chris Palmer <chris@red-oxide.org>
2026-06-26 10:28:08 -0400

    input updates and remove 32bit from pipewire
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -8,10 +8,7 @@ _: {
       security.rtkit.enable = true;
       services.pipewire = {
         enable = true;
-        alsa = {
-          enable = true;
-          support32Bit = true;
-        };
+        alsa.enable = true;
         pulse.enable = true;
         jack.enable = true;
       };
