c9fa3b37d48178f1b84ee6e5e9d6799541acaa60
Chris Palmer <chris@red-oxide.org>
2026-08-22 16:11:32 -0400

    change helix key bindings
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/term/helix/keybinds.nix
+++ modules/term/helix/keybinds.nix
@@ -1,13 +1,16 @@
 _: {
   flake.modules.homeManager.base = {
     programs.helix.settings.keys.normal = {
-      "tab" = "goto_next_buffer";
-      "S-tab" = "goto_previous_buffer";
-      "C-x" = ":buffer-close";
-      "X" = [
-        "extend_line_up"
-        "extend_to_line_bounds"
-      ];
+      "space" = {
+        left = ":buffer-previous";
+        right = ":buffer-next";
+      };
+      n = {
+        a = ":buffer-close-all";
+        c = ":buffer-close";
+        f = ":buffer-close!";
+        s = ":write";
+      };
     };
   };
 }
