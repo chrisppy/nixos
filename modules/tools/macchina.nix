4a85c57a83dee80cd1ac2bcd0021b8be89f14502
Chris Palmer <chris@red-oxide.org>
2026-09-05 11:54:27 -0400

    add macchina config
    Signed-off-by: Chris Palmer <chris@red-oxide.org>

--- modules/tools/macchina.nix
+++ modules/tools/macchina.nix
@@ -2,6 +2,23 @@ _: {
   flake.modules.homeManager.base = {
     programs.macchina = {
       enable = true;
+      settings = {
+        show = [
+          "Host"
+          "Machine"
+          "Kernel"
+          "Distribution"
+          "WindowManager"
+          "Packages"
+          "Terminal"
+          "Uptime"
+          "Processor"
+          "ProcessorLoad"
+          "Memory"
+          "GPU"
+          "DiskSpace"
+        ];
+      };
     };
   };
 }
